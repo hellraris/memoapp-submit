56873fdf729103da9fcf1fff20431c2a058e74ec
KyeongsangNa <hellraris@gmail.com>
2019-10-12 01:51:40 +0900

    ADD: 에러메세지 정의

---
+++ memoapp-front/src/messages/errorMessage.js
@@ -0,0 +1,16 @@
+
+export const GET_MEMO_LIST_FAILURE_MESSAGE = '메모리스트 불러오기에 실패하였습니다';
+export const GET_MEMO_FAILURE_MESSAGE = '메모 불러오기에 실패하였습니다';
+export const CREATE_MEMO_FAILURE_MESSAGE = '메모작성에 실패하였습니다';
+export const UPDATE_MEMO_FAILURE_MESSAGE = '메모수정에 실패하였습니다';
+export const REMOVE_MEMO_FAILURE_MESSAGE = '메모삭제에 실패하였습니다';
+export const REMOVE_MEMOS_FAILURE_MESSAGE = '일괄 메모삭제에 실패하였습니다';
+
+export const GET_LABEL_LIST_FAILURE_MESSAGE = '라벨리스트 불러오기에 실패하였습니다';
+export const GET_LABEL_FAILURE_MESSAGE = '라벨 불러오기에 실패하였습니다';
+export const CREATE_LABEL_FAILURE_MESSAGE = '라벨작성에 실패하였습니다';
+export const UPDATE_LABEL_FAILURE_MESSAGE = '라벨수정에 실패하였습니다';
+export const ADD_LABEL_MEMOS_FAILURE_MESSAGE = '라벨지정에 실패하였습니다';
+export const REMOVE_LABEL_FAILURE_MESSAGE = '라벨삭제에 실패하였습니다';
+export const REMOVE_LABEL_MEMOS_FAILURE_MESSAGE = '라벨메모삭제에 실패하였습니다';
+export const REMOVE_ALL_LABEL_MEMOS_FAILURE_MESSAGE = '라벨메모 일괄삭제에 실패하였습니다';
